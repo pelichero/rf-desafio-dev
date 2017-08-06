c8273d24b0427299dad8f9ca2d0e561d1241fe19
Felipe Pelichero <Felipe Pelichero>
2017-08-05 21:26:11 -0300

    Iniciando controller rest

--- testdev-api/src/main/java/br/com/cvc/reservafacil/testdevapi/TestdevApiApplication.java
+++ testdev-api/src/main/java/br/com/cvc/reservafacil/testdevapi/TestdevApiApplication.java
@@ -2,8 +2,10 @@ package br.com.cvc.reservafacil.testdevapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"br.com.cvc.reservafacil.testdevapi.controller","br.com.cvc.reservafacil.testdevcore"})
 public class TestdevApiApplication {
 
 	public static void main(String[] args) {
