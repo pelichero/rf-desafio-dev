d8f1b5fe288212b4b1774242846d9c7ca23cff02
Felipe Pelichero <pelichero_13@hotmail.com>
2017-09-10 21:12:21 -0300

    testando

--- testdev-core/src/main/java/br/com/cvc/reservafacil/poccenter/streams/StreamTest.java
+++ testdev-core/src/main/java/br/com/cvc/reservafacil/poccenter/streams/StreamTest.java
@@ -1,9 +1,20 @@
 package br.com.cvc.reservafacil.poccenter.streams;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StreamTest {
 
     public static void main(String[] args) {
-        
+        List<String> palavras = new ArrayList<>();
+        palavras.add("felipe");
+        palavras.add("karina");
+        palavras.add("elaine");
+
+
+        palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+
+        palavras.forEach(s -> System.out.println(s));
     }
 
 }
